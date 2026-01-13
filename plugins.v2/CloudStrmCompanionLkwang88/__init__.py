c3834007766c3621cf52cff046d98d2a8b944199
Lkwang88 <jackfox888888@gmail.com>
2026-01-13 14:46:41 +0800

    2.0.1

--- plugins.v2/CloudStrmCompanionLkwang88/__init__.py
+++ plugins.v2/CloudStrmCompanionLkwang88/__init__.py
@@ -57,7 +57,7 @@ class Lkwang88(_PluginBase):
     # 插件图标
     plugin_icon = "cloudcompanion.png"
     # 插件版本
-    plugin_version = "2.0.0"
+    plugin_version = "2.0.1"
     # 插件作者
     plugin_author = "lkwang88"
     # 作者主页
