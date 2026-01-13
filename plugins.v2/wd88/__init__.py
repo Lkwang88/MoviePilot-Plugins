5db6d717d1fa92dd72d67f9428b719874c813203
Lkwang88 <jackfox888888@gmail.com>
2026-01-13 15:30:36 +0800

    fix

--- plugins.v2/wd88/__init__.py
+++ plugins.v2/wd88/__init__.py
@@ -57,7 +57,7 @@ class wd88(_PluginBase):
     # 插件图标
     plugin_icon = "cloudcompanion.png"
     # 插件版本
-    plugin_version = "2.1.0"
+    plugin_version = "2.1.1"
     # 插件作者
     plugin_author = "lkwang88"
     # 作者主页
